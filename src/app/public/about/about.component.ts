d186c370720cd27e3b04007e3d202de31d948f3e
Maicon <maicon.fang@gmail.com>
2024-08-16 12:12:23 -0300

    In this commit, the English version and the EN and PT-BR files were modified.

--- src/app/public/about/about.component.ts
+++ src/app/public/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  skills: string[] = [];
+
+  constructor(private translate: TranslateService) { }
 
   ngOnInit() {
+    this.translate.get('Experience.skills').subscribe((data: any) => {
+      this.skills = data[0].skills;
+    });
   }
 
 }
